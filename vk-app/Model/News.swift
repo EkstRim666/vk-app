45df2230aec676c82fcc0dbed3921d1032a00cab
Andrey Yusupov <electrooptic@yandex.ru>
2018-09-10 19:44:42 +0700

    Add news interface

---
+++ vk-app/Model/News.swift
@@ -0,0 +1,9 @@
+//
+//  News.swift
+//  vk-app
+//
+//  Created by Andrey Yusupov on 10/09/2018.
+//  Copyright © 2018 Andrey Yusupov. All rights reserved.
+//
+
+import Foundation
---
+++ vk-app/NewsTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewsTableViewController.swift
+//  vk-app
+//
+//  Created by Andrey Yusupov on 10/09/2018.
+//  Copyright © 2018 Andrey Yusupov. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
