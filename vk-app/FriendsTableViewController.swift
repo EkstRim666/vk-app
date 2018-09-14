69802123665495bee941171227a18066f08af610
Andrey Yusupov <electrooptic@yandex.ru>
2018-09-14 13:39:55 +1200

    Add pair friends with data and view friends in table

--- vk-app/FriendsTableViewController.swift
+++ vk-app/FriendsTableViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import RealmSwift
 
 class FriendsTableViewController: UITableViewController {
+    
+    private var friends: [User] = []
+    private var tokenFriends: NotificationToken?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +20,34 @@ class FriendsTableViewController: UITableViewController {
         tableView.rowHeight = 55
         
         Service.getFriends(userId: Service.getUserId())
+        pairTableFriendsAndData()
+    }
+    
+    private func pairTableFriendsAndData() {
+        guard let friends = DataWorker.loadUserData()
+            else { return }
+        self.friends = Array(friends)
+        tokenFriends = friends.observe {[weak self] (changes) in
+            guard let strongSelf = self
+                else { return }
+            switch changes {
+            case .initial:
+                strongSelf.tableView.reloadData()
+            
+            case let .update(results, deletions, insetion, modifications):
+            strongSelf.friends = Array(results)
+            strongSelf.tableView.beginUpdates()
+            strongSelf.tableView.insertRows(at: insetion.map({IndexPath(row: $0, section: 0)}), with: .automatic)
+            strongSelf.tableView.deleteRows(at: deletions.map({IndexPath.init(row: $0, section: 0)}), with: .automatic)
+            strongSelf.tableView.reloadRows(at: modifications.map({IndexPath.init(row: $0, section: 0)}), with: .automatic)
+            strongSelf.tableView.endUpdates()
+            
+            case .error(let error):
+            print(error)
+            assertionFailure()
+                
+            }
+        }
     }
 
     // MARK: - Table view data source
@@ -24,6 +56,18 @@ class FriendsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return friends.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Friend", for: indexPath) as! FriendTableViewCell
+        let user = friends[indexPath.row]
+        cell.setName(text: user.name)
+        return cell
+    }
+    
+    //MARK: - Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
